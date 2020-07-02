b87528a03353b79e86cf648dde4e1c7f670b199f
danugimnastian <38465916+danugimnastian@users.noreply.github.com>
2020-07-02 14:10:18 +0700

    tugas

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('indexbaru');
+});
+
+Route::get('/blank', function() {
+    return view('blank');
+});
+Route::get('/data-tables', function () {
+    return view('contents.data-tables');
+});
